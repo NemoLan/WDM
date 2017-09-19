eb2738fdab0887107d1d94dba80694ea7743ed82
NemoLan <lantian435@163.com>
2017-09-19 13:58:03 +0800

    add some zhushi for name.select

--- WDM/JobInfoSpider.py
+++ WDM/JobInfoSpider.py
@@ -15,11 +15,13 @@ for i in range(22,25):
     url = urljoin(Career_url, 'getjobFairCalMeetingInfoAction.dhtml?selectDate=2017-09-%02d' % i)
     response = urlopen(url)
     html = bs(response.read(),'html.parser')#指定parser解析器"html.parser"
-    CompanyNames = html.findAll('a')#, {"class":"comment"}
+    CompanyNames = html.findAll('a')
     Csstimes = html.findAll('span',{"class":"csstime"})
     Cssadresses = html.findAll('span',{"class":"cssadress"})
     for name in CompanyNames:
         Cpy_url.append(urljoin(Career_url, name["href"]))
+        #上面一句中name中只有a一个tag，只能直接定位"herf"，
+        #所以使用name.select("a")[0]["herf"]是不能select到"a"的
         Cpy_Names.append(name.get_text())
     for time in Csstimes:
         Cpy_Times.append(time.get_text())
